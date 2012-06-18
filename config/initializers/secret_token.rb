6fa6e19dd66f00b944d99d49901c2161d2ee6e9a
Artem <info@saveart.su>
2012-06-19 02:27:25 +0400

    Rails 3.2.6 Project generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForroPiratesMusic::Application.config.secret_token = 'b48b0988f3ea94f4121fd7908c79c85db7103d89a74f71d8c877870e54ceed341014e2685a05590da227569b4ef107a4803edf8bd7cad71b30398e7b5c4fb0c6'
